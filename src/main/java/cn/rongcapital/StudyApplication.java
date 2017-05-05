f3c2746b819c25cc4b28565071466d5723910315
wangweiqiang <wangweiqiang@rongcapital.cn>
2017-05-05 18:00:28 +0800

    change tomcat server port

--- src/main/java/cn/rongcapital/StudyApplication.java
+++ src/main/java/cn/rongcapital/StudyApplication.java
@@ -2,11 +2,18 @@ package cn.rongcapital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
 @SpringBootApplication
-public class StudyApplication {
+public class StudyApplication implements EmbeddedServletContainerCustomizer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApplication.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+		configurableEmbeddedServletContainer.setPort(8066);
 	}
 }
